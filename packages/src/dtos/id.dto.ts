432780de97cd1ff091a7b9403fe00c9fdd5a275e
Lora Seguin <lora80123@gmail.com>
2026-01-15 14:05:25 +0100

    feat(papyrus): update project

--- packages/src/dtos/id.dto.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const idSchema = z.object({
-  id: z.uuid("Le format de l'id est invalide"),
-});
-
-export type Id = z.infer<typeof idSchema>;
--- packages/src/error.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const errorSchema = z.object({
-  error: z.string(),
-  message: z.string(),
-});

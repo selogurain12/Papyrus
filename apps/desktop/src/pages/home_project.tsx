b035460ed8c804135ac6c3cb4eb6bf2c19ecfc6c
Lora Seguin <lora80123@gmail.com>
2026-03-07 14:58:21 +0100

    feat(papyrus/webui): change ui of sidebar - #36

--- apps/desktop/src/pages/home_project.tsx
+++ apps/desktop/src/pages/home_project.tsx
@@ -1,14 +1,20 @@
 import { Sidebar } from "../components/sidebar";
 import { Header } from "../components/header";
 import { projectHomeRoute } from "../routes/project/index.route";
+import { Outlet } from "@tanstack/react-router";
 
 export function HomeProject() {
   const { name } = projectHomeRoute.useParams();
   return (
-    <div className="flex h-screen bg-gray-50">
-      <Sidebar name={name} />
-      <main className="w-full">
+    <div className="flex h-full min-h-screen bg-gray-50">
+      <div className="sticky top-0 h-screen overflow-hidden">
+        <Sidebar name={name} />
+      </div>
+      <main className="flex-1 flex flex-col overflow-hidden">
         <Header name={name} />
+        <div className="flex-1 overflow-y-auto">
+          <Outlet />
+        </div>
       </main>
     </div>
   );

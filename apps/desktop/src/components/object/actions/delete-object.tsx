f8aa85a2409a8874061c8629672a2921b222f49c
Lora Seguin <lora80123@gmail.com>
2026-03-08 19:55:41 +0100

    feat(papyrus/webui): delete form - #67

---
+++ apps/desktop/src/components/object/actions/delete-object.tsx
@@ -0,0 +1,86 @@
+import { ObjectDto } from "@papyrus/source";
+import { useQueryClient } from "@tanstack/react-query";
+import { client } from "../../../utils/client/client";
+import { toast } from "sonner";
+import { isFetchError } from "@ts-rest/react-query/v5";
+import { AlertDialog } from "../../ui/alert-dialog/alert-dialog";
+import { AlertDialogContent } from "../../ui/alert-dialog/alert-dialog-content";
+import { AlertDialogHeader } from "../../ui/alert-dialog/alert-dialog-header";
+import { AlertDialogTitle } from "../../ui/alert-dialog/alert-dialog-title";
+import { AlertDialogDescription } from "../../ui/alert-dialog/alert-dialog-description";
+import { AlertDialogFooter } from "../../ui/alert-dialog/alert-dialog-footer";
+import { MotionAlertDialogCancelWrapper } from "../../ui/alert-dialog/motion/cancel-wrapper.motion";
+import { MotionAlertDialogActionWrapper } from "../../ui/alert-dialog/motion/action-wrapper.motion";
+import { useProject } from "../../../context/project-provider";
+import { objectRoute } from "../../../routes/object/index.route";
+import { useNavigate } from "@tanstack/react-router";
+
+interface ObjectDeleteActionsProps {
+  onClose?: () => void;
+  open: boolean;
+  // eslint-disable-next-line no-unused-vars
+  setOpen: (open: boolean) => void;
+  object: ObjectDto;
+  clearSelection: () => void;
+}
+
+export function ObjectDeleteActions({
+  object,
+  open,
+  setOpen,
+  onClose = undefined,
+  clearSelection,
+}: ObjectDeleteActionsProps) {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { currentProject } = useProject();
+  if (currentProject === null) {
+    toast.error("Projet non sélectionné");
+    return null;
+  }
+
+  const { mutate } = client.object.softDelete.useMutation({
+    onSuccess: () => {
+      toast.success("L'objet a été supprimé avec succès");
+      void queryClient.invalidateQueries({
+        queryKey: ["object.getAll"],
+      });
+      clearSelection();
+      onClose?.();
+    },
+
+    onError: (error) => {
+      if (isFetchError(error)) {
+        toast.error(error.message);
+      } else {
+        toast.error("Une erreur est survenue");
+      }
+    },
+  });
+
+  return (
+    <AlertDialog onOpenChange={setOpen} open={open}>
+      <AlertDialogContent className="sm:max-w-[800px] sm:max-h-[80%] bg-white rounded-lg p-8">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Supprimer un objet</AlertDialogTitle>
+          <AlertDialogDescription>
+            Êtes-vous sûr de vouloir supprimer cet objet ?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <MotionAlertDialogCancelWrapper onClick={() => setOpen(false)} />
+          <MotionAlertDialogActionWrapper
+            onClick={() => {
+              mutate({ params: { id: object.id, projectId: currentProject.id } });
+              navigate({
+                to: objectRoute.to,
+                params: { name: currentProject.title },
+              });
+            }}
+          />
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}

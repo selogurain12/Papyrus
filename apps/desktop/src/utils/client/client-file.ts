3605f0895245e28ea287668b8f9e400eb827cf96
Lora Seguin <lora80123@gmail.com>
2026-04-11 16:17:57 +0200

    feat(papyrus/webui): create new client for update file - #69

---
+++ apps/desktop/src/utils/client/client-file.ts
@@ -0,0 +1,45 @@
+import { type ApiFetcherArgs, type AppRouter, tsRestFetchApi } from "@ts-rest/core";
+import { initTsrReactQuery } from "@ts-rest/react-query/v5";
+
+import { papyrusContract } from "@papyrus/source";
+
+const apiUrl = "http://localhost:3000";
+
+function getIdTokenAsync(): string | null {
+  try {
+    return localStorage.getItem("token");
+  } catch (error) {
+    console.error("Erreur lors de la récupération du token :", error);
+    return null;
+  }
+}
+
+export function createClient<Tcontract extends AppRouter>(contract: Tcontract) {
+  const idToken = getIdTokenAsync();
+
+  const tokenAuthorization = idToken === null || idToken === undefined ? "" : `Bearer ${idToken}`;
+
+  return initTsrReactQuery(contract, {
+    baseUrl: apiUrl,
+
+    baseHeaders: {
+      Authorization: tokenAuthorization,
+    },
+
+    api: async (args: ApiFetcherArgs) => {
+      const idToken = getIdTokenAsync();
+      if (idToken !== null && idToken !== undefined) {
+        const header = {
+          Authorization: `Bearer ${idToken}`,
+        };
+
+        return await tsRestFetchApi({ ...args, headers: header });
+      }
+
+      throw new Error("No id token");
+    },
+  });
+}
+export type PapyrusClient = ReturnType<typeof createClient<typeof papyrusContract>>;
+
+export const clientFile: PapyrusClient = createClient(papyrusContract);

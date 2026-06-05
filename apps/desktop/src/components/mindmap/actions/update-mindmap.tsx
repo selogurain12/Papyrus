519ae19d26322fe6029572ccca65c7c2ddd331d5
Lora Seguin <lora80123@gmail.com>
2026-06-05 17:14:50 +0200

    feat(papyrus/webui): update mindmap - #94

---
+++ apps/desktop/src/components/mindmap/actions/update-mindmap.tsx
@@ -0,0 +1,128 @@
+import { useEffect, useRef } from "react";
+import MindElixir, { MindElixirData } from "mind-elixir";
+import "mind-elixir/style.css";
+import { client } from "../../../utils/client/client";
+import { queryKeys, updateMindMapSchema } from "@papyrus/source";
+import { useProject } from "../../../context/project-provider";
+import { useNavigate } from "@tanstack/react-router";
+import { mindmapRoute, updateMindmapRoute, viewMindmapRoute } from "../../../routes/mindmap/index.route";
+import { Button } from "../../ui/button";
+import { toast } from "sonner";
+import { queryClient } from "../../../context/query-client";
+import { isFetchError } from "@ts-rest/react-query/v5";
+
+export function UpdateMindMap() {
+  const { currentProject } = useProject();
+  const navigate = useNavigate();
+  const { id } = updateMindmapRoute.useParams();
+  const mindRef = useRef<InstanceType<typeof MindElixir> | null>(null);
+
+  const { data } = client.mindmap.get.useQuery({ 
+    queryKey: queryKeys.mindmap.get({pathParams: { id, projectId: currentProject?.id ?? "" }}),
+    queryData: {
+      params: { projectId: currentProject?.id ?? "", id },
+    },
+   });
+
+  const mindmap = data?.body;
+
+  useEffect(() => {
+    if (!mindmap) return;
+
+    const instance = new MindElixir({
+      el: "#viewer-map",
+      direction: MindElixir.LEFT,
+      draggable: true,
+      contextMenu: true,
+      toolBar: true,
+      keypress: true,
+    });
+
+    instance.init(mindmap.data as MindElixirData);
+
+    mindRef.current = instance;
+  }, [mindmap]);
+
+   const { mutate } = client.mindmap.update.useMutation({
+    onSuccess: (data) => {
+      toast.success("Carte mentale modifiée avec succès !");
+      void queryClient.invalidateQueries({ queryKey: queryKeys.mindmap.getAll({pathParams: { projectId: currentProject?.id ?? "" }}) });
+      void queryClient.invalidateQueries({
+        queryKey: queryKeys.mindmap.get({
+          pathParams: { projectId: currentProject?.id ?? "", id: data.body.id },
+        }),
+      });
+      void navigate({to: mindmapRoute.to, params: { name: currentProject?.title ?? "" }});
+    },
+    onError: (error) => {
+      if (isFetchError(error)) {
+        console.error("Fetch error:", error);
+        toast.error(error.message);
+      } else {
+        toast.error("Une erreur est survenue");
+      }
+    },
+  });
+
+    async function handleSubmit() {
+      if (!mindRef.current) {
+        toast.error("MindElixir n'est pas initialisé.");
+        return;
+      }
+      if (!currentProject) {
+        toast.error("Aucun projet sélectionné.");
+        return;
+      }
+  
+      const data = mindRef.current.getData();
+      const parsed = updateMindMapSchema.safeParse({
+        title: "Nouvelle carte mentale",
+        project: currentProject,
+        data,
+      });
+  
+      if (!parsed.success) {
+        toast.error("Veuillez remplir correctement les champs.");
+        return;
+      }
+  
+      mutate({
+        body: {
+          title: data.nodeData.topic,
+          data,
+          project: currentProject,
+        },
+        params: {projectId: currentProject.id, id},
+      });
+    }
+
+  if (!mindmap) return <div>Loading...</div>;
+
+  return (
+    <div className="p-6">
+      <div className="space-y-6">
+        <div className="flex justify-between items-center">
+          <h2 className="text-2xl font-semibold">{mindmap.title}</h2>
+          <Button
+            onClick={() => void navigate({ to: mindmapRoute.to, params: { name: currentProject?.title ?? "" } })}
+            className="px-6 py-3 rounded-lg border border-gray-400 hover:bg-gray-400 transition"
+          >
+            Fermer
+          </Button>
+        </div>
+
+        <div className="bg-white shadow rounded-xl p-4">
+          <div id="viewer-map" style={{ height: "500px", width: "100%" }} />
+        </div>
+        <div className="mt-6 flex w-full">
+    <Button
+          onClick={handleSubmit}
+          className="px-6 py-3 w-full bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+        >
+          Modifier la carte mentale
+        </Button>
+      </div>
+      </div>
+    </div>
+  );
+}

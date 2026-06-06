c344929064a0c35354a9a886fce5b511ba66e74f
Lora Seguin <lora80123@gmail.com>
2026-06-06 22:31:55 +0200

    feat(papyrus/webui): delete chapter - #100

---
+++ apps/desktop/src/components/chapter/actions/chapter/delete-chapter.tsx
@@ -0,0 +1,86 @@
+/* eslint-disable max-len */
+/* eslint-disable no-unused-vars */
+import { ChapterDto, queryKeys } from "@papyrus/source";
+import { useQueryClient } from "@tanstack/react-query";
+import { isFetchError } from "@ts-rest/react-query/v5";
+import { toast } from "sonner";
+import { useProject } from "../../../../context/project-provider";
+import { client } from "../../../../utils/client/client";
+import { AlertDialogFooter } from "../../../ui/alert-dialog/alert-dialog-footer";
+import { AlertDialogHeader } from "../../../ui/alert-dialog/alert-dialog-header";
+import { MotionAlertDialogActionWrapper } from "../../../ui/alert-dialog/motion/action-wrapper.motion";
+import { MotionAlertDialogCancelWrapper } from "../../../ui/alert-dialog/motion/cancel-wrapper.motion";
+import { AlertDialog } from "../../../ui/alert-dialog/alert-dialog";
+import { AlertDialogContent } from "../../../ui/alert-dialog/alert-dialog-content";
+import { AlertDialogTitle } from "../../../ui/alert-dialog/alert-dialog-title";
+import { AlertDialogDescription } from "../../../ui/alert-dialog/alert-dialog-description";
+
+interface ChapterDeleteActionsProps {
+  onClose?: () => void;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  chapter: ChapterDto;
+  clearSelection: () => void;
+}
+
+export function ChapterDeleteActions({
+  chapter,
+  open,
+  setOpen,
+  onClose = undefined,
+  clearSelection,
+}: ChapterDeleteActionsProps) {
+  const queryClient = useQueryClient();
+  const { currentProject } = useProject();
+  if (currentProject === null) {
+    toast.error("Projet non sélectionné");
+    return null;
+  }
+
+  const { mutate } = client.chapter.softDelete.useMutation({
+    onSuccess: () => {
+      toast.success("Le chapitre a été supprimé avec succès");
+      void queryClient.invalidateQueries({
+        queryKey: queryKeys.chapter.getAll({
+          pathParams: { projectId: currentProject?.id ?? "" },
+        }),
+      });
+      void queryClient.invalidateQueries({
+        queryKey: queryKeys.chapter.getByPart({
+          pathParams: { projectId: currentProject?.id ?? "", partId: chapter.part.id },
+        }),
+      });
+      clearSelection();
+      onClose?.();
+    },
+    onError: (error) => {
+      if (isFetchError(error)) {
+        toast.error(error.message);
+      } else {
+        toast.error("Une erreur est survenue");
+      }
+    },
+  });
+
+  return (
+    <AlertDialog onOpenChange={setOpen} open={open}>
+      <AlertDialogContent className="sm:max-w-200 sm:max-h-[80%] bg-white rounded-lg p-8">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Supprimer la chapterie</AlertDialogTitle>
+          <AlertDialogDescription>
+            Êtes-vous sûr de vouloir supprimer « {chapter.title} » ? Cette action est irréversible.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <MotionAlertDialogCancelWrapper onClick={() => setOpen(false)} />
+          <MotionAlertDialogActionWrapper
+            onClick={() => {
+              mutate({ params: { id: chapter.id, projectId: currentProject.id } });
+            }}
+          />
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}

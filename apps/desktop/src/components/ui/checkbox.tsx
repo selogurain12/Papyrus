a6d633d6c8d52dec7b336cfd624d4f1956d0408d
Lora Seguin <lora80123@gmail.com>
2026-06-28 21:23:59 +0200

    feat(papyrus/webui): update component - #16

--- apps/desktop/src/components/ui/checkbox.tsx
+++ apps/desktop/src/components/ui/checkbox.tsx
@@ -11,7 +11,7 @@ const checkbox = forwardRef<
   <Root
     className={cn(
       // eslint-disable-next-line max-len
-      "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+      "peer relative flex size-4 shrink-0 items-center justify-center rounded-sm border border-input transition-colors outline-none group-has-disabled/field:opacity-50 after:absolute after:-inset-x-3 after:-inset-y-2 focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 aria-invalid:aria-checked:border-primary dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 data-[state=checked]:border-primary data-[state=checked]:bg-black data-[state=checked]:text-white dark:data-[state=checked]:bg-primary",
       className
     )}
     ref={reference}

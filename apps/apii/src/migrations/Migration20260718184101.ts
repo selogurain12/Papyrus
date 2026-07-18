da8a973aef3829230eddf1a7a1602787fc5d4b7f
Lora Seguin <lora80123@gmail.com>
2026-07-18 22:02:16 +0200

    feat(papyrus/api): update migration - #185

--- apps/apii/src/migrations/Migration20260718184101.ts
+++ apps/apii/src/migrations/Migration20260718184101.ts
@@ -6,15 +6,32 @@ import { Migration } from "@mikro-orm/migrations";
 
 export class Migration20260718184101 extends Migration {
   override async up(): Promise<void> {
-    this.addSql('alter table "event_entity" add column "chapter_id" uuid null;');
+    this.addSql('alter table "event_entity" add column if not exists "chapter_id" uuid null;');
     this.addSql(
-      "alter table \"event_entity\" add constraint \"event_entity_chapter_id_foreign\" foreign key (\"chapter_id\") references \"chapter_entity\" (\"id\") on update cascade on delete cascade;"
+      `do $$
+      begin
+        if not exists (
+          select 1
+          from information_schema.table_constraints
+          where table_name = 'event_entity'
+            and constraint_name = 'event_entity_chapter_id_foreign'
+        ) then
+          alter table "event_entity"
+            add constraint "event_entity_chapter_id_foreign"
+            foreign key ("chapter_id")
+            references "chapter_entity" ("id")
+            on update cascade
+            on delete cascade;
+        end if;
+      end $$;`
     );
   }
 
   override async down(): Promise<void> {
-    this.addSql('alter table "event_entity" drop constraint "event_entity_chapter_id_foreign";');
+    this.addSql(
+      'alter table "event_entity" drop constraint if exists "event_entity_chapter_id_foreign";'
+    );
 
-    this.addSql('alter table "event_entity" drop column "chapter_id";');
+    this.addSql('alter table "event_entity" drop column if exists "chapter_id";');
   }
 }

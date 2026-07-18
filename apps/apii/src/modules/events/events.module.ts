0f75a9591566531b544d34ef18d34ffb2ff572a9
Lora Seguin <lora80123@gmail.com>
2026-07-18 21:17:24 +0200

    feat(papyrus/api): update import - #184

--- apps/apii/src/modules/events/events.module.ts
+++ apps/apii/src/modules/events/events.module.ts
@@ -2,13 +2,14 @@ import { MikroOrmModule } from "@mikro-orm/nestjs";
 import { Module } from "@nestjs/common";
 import { ProjectModule } from "../projects/projects.module";
 import { HistoryModule } from "../history/history.module";
+import { ChapterModule } from "../chapters/chapters.module";
 import { EventEntity } from "./events.entity";
 import { EventController } from "./events.controller";
 import { EventService } from "./events.service";
 import { EventMapper } from "./events.mapper";
 
 @Module({
-  imports: [MikroOrmModule.forFeature([EventEntity]), ProjectModule, HistoryModule],
+  imports: [MikroOrmModule.forFeature([EventEntity]), ProjectModule, HistoryModule, ChapterModule],
   controllers: [EventController],
   providers: [EventService, EventMapper],
   exports: [EventService, EventMapper],

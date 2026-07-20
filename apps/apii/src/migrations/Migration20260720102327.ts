8ee49efe03f3fd040240521d1004bfbd6dac9493
Lora Seguin <lora80123@gmail.com>
2026-07-20 13:56:44 +0200

    feat(papyrus/api): update deploy yaml - #160

--- apps/apii/src/migrations/Migration20260720102327.ts
+++ apps/apii/src/migrations/Migration20260720102327.ts
@@ -4,10 +4,12 @@ import { Migration } from "@mikro-orm/migrations";
 
 export class Migration20260720102327 extends Migration {
   override async up(): Promise<void> {
-    this.addSql("alter table \"project_entity\" add column \"cover_link\" varchar(255) null;");
+    this.addSql(
+      "alter table \"project_entity\" add column if not exists \"cover_link\" varchar(255) null;"
+    );
   }
 
   override async down(): Promise<void> {
-    this.addSql("alter table \"project_entity\" drop column \"cover_link\";");
+    this.addSql("alter table \"project_entity\" drop column if exists \"cover_link\";");
   }
 }
